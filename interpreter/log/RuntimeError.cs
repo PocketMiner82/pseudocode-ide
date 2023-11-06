ed306dcc7da22abd36ed84303d6ae5bdd4953fb6
PocketMiner82 <pocket.miner@gmx.de>
2023-11-06 15:01:52 +0100

    re-structure code, implement csharpcode executor

---
+++ interpreter/log/RuntimeError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pseudocode_ide.interpreter.sequences
+{
+    public class RuntimeError : Exception
+    {
+        public int line { get; set; }
+
+        public RuntimeError(int line, string message) : base(message)
+        {
+            this.line = line;
+        }
+    }
+}
