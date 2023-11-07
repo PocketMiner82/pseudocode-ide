a6a1f7d4ed3e448317a041cdd0db03a9d80250ab
PocketMiner82 <pocket.miner@gmx.de>
2023-11-07 08:36:50 +0100

    fix scaling

--- Program.cs
+++ Program.cs
@@ -11,9 +11,13 @@ namespace pseudocodeIde
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PseudocodeIDEForm());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
