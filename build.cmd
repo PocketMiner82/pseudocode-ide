1afbbbd22972bed0a8e2306b0a30c7dfea7abb7e
PocketMiner82 <pocket.miner@gmx.de>
2024-04-03 18:36:32 +0200

    ci: prevent zipping on pull request build, as upload-artifact is handling the zipping

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 @echo off
 nuget restore pseudocodeIde.sln
 msbuild pseudocodeIde.sln -t:rebuild -property:Configuration=Release
-del "pseudocode-ide.zip"
-7z a pseudocode-ide.zip .\bin\Release\*
+if not "%1"=="nozip" (
+    del "pseudocode-ide.zip"
+    7z a pseudocode-ide.zip .\bin\Release\*
+)
